1d7836b186a10c9d578a4727a5f0b5081633f6c5
Oskar <oskarlucewicz@gmail.com>
2022-05-16 13:22:38 +0200

    login screen resized

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="flex w-screen h-screen bg-white flex-grow p-32 font-montserrat overflow-hidden">
     <div class="flex flex-col flex-grow h-full items-center justify-center">
-      <div class="bg-white w-1/3 rounded-3xl shadow-lg flex flex-col border border-gray-100">
-        <div class="flex flex-row justify-center m-5">
-          <img src="../assets/systems.svg" alt class="h-20 w-44 object-fill p-1" />
-          <div class="self-center">
-            <h2 class="text-right font-medium tracking-wider leading-5">
-              <span class="tracking-normal">WEBGENETICSS</span> Interpolate
+      <div class="bg-white w-1/2 2xl:w-1/2 3xl:w-2/5 rounded-3xl shadow-lg flex flex-col border border-gray-100 ">
+        <div class="bg-gradient-to-tl from-gray-50 via-gray-100 to-gray-100 mb-7 rounded-t-3xl h-40 ">
+        <div class="flex flex-row justify-between h-full">
+          <img src="../assets/systems.svg" alt class="h-full w-3/5 3xl:w-2/3 object-cover overflow-hidden" />
+          <div class="self-center h-full w-1/3 flex-shrink-0 p-2.5 2xl:p-3 3xl:p-3  flex flex-col justify-center items-end overflow-hidden">
+            <h2 class="text-right font-medium tracking-wider leading-5 text-xs 2xl:text-sm 3xl:text-base">
+              <span class="tracking-wider font-semibold text-sm 2xl:text-base 3xl:text-lg">WEBGENETICSS</span> Interpolate
             </h2>
-            <h2 class="text-right font-medium tracking-wider leading-5">Management System</h2>
-            <h2 class="text-right text-xs font-medium tracking-wide leading-4">{{ version }}</h2>
+            <h2 class="text-right font-medium tracking-wider leading-5 text-xs 2xl:text-sm 3xl:text-base">Management System</h2>
+            <h2 class="text-right text-3xs 2xl:text-2xs 3xl:text-xs font-medium tracking-wide leading-4">{{ version }}</h2>
             <p class="text-gray-600 font-normal text-xs text-right tracking-wide">
               ♻ CO2-FREE
-              <span class="font-light tracking-widest text-2xs">for our planet</span>
+              <span class="font-medium tracking-widest text-3xs 2xl:text-2xs 3xl:text-xs">for our planet</span>
             </p>
           </div>
+        </div>
         </div>
         <form @submit.prevent="login" class="flex flex-col">
           <input required id="email" type="text" placeholder="email lub login" v-model="email"
